018db326e843608ffe536d155eff681262abeafc
MirkoBoschHHS <79100495+MirkoBoschHHS@users.noreply.github.com>
2021-10-13 19:16:27 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import pandas as pd
-# import numpy as np
-# import requests
-# import plotly.figure_factory as ff
-# import plotly.express as px
-# import io
-# import time
-# import plotly.graph_objects as go
-# from plotly.subplots import make_subplots
-# import os
+import numpy as np
+import requests
+import plotly.figure_factory as ff
+import plotly.express as px
+import io
+import time
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import os
 from streamlit_folium import folium_static
-# import folium
-# from glob import glob
+import folium
+
 
 import Get_data
 import Figuren
